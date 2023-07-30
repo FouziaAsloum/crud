df83e63a07f9a0cc2e9d01d0e9bdc8b8de644a4d
SanaSanss <fouzia.asloum@outlook.fr>
2023-07-30 16:16:39 +0200

    helped

--- client/script.js
+++ client/script.js
@@ -8,11 +8,48 @@ fetch(url)
       .then(data => {
         console.log(data.data)
         data.forEach(element => {
-          console.log(element);
+          resultat.innerHTML += `<tr>
+                                    <td>${element.nom}</td>
+                                    <td>${element.prenom}</td>
+                                    <td>${element.adresse}</td>
+                                    <td>${element.code_postal}</td>
+                                    <td>${element.ville}</td>
+                                    <td>${element.telephone}</td>
+                                    <td>${element.email}</td>
+                                    <td><button class="btngreen" id="${element.id}">Modifier </button></td>
+                                    <td><button class="btnred" id="${element.id}">Supprimer </button></td>
+                                    </tr>`;
+        });
 
-        })
       })
   })
 
   .catch(error => console.error(error));
 console.log(xiao);
+
+
+function addUser(event) {
+  event.preventDefault(); // Empêche le formulaire de se soumettre normalement
+
+  // Récupérer les valeurs du formulaire
+  const nom = document.getElementById("nom").value;
+  const prenom = document.getElementById("prenom").value;
+  const adresse = document.getElementById("adress").value;
+  const code_postal = document.getElementById("cp").value;
+  const ville = document.getElementById("ville").value;
+  const telephone = document.getElementById("tel").value;
+  const email = document.getElementById("email").value;
+
+  // Créer un nouvel élément de liste
+  const li = document.createElement("li");
+  li.textContent = `Nom ${nom}, Prénom ${prenom}, Adresse ${adresse}, Code postal ${code_postal}, Ville ${ville}, Téléphone ${telephone}, Email ${email}`;
+
+  // Ajouter l'élément de liste à la liste des utilisateurs
+  document.getElementById("userList").appendChild(li);
+
+  // Réinitialiser le formulaire
+  document.getElementById("userForm").reset();
+}
+
+// Écouter l'événement de soumission du formulaire et appeler la fonction addUser
+document.getElementById("userForm").addEventListener("submit", addUser);
